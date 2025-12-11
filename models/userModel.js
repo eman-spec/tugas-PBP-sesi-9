b8fc1831e2ba83919d83d9f4b59fa964e89afd0d
eman-spec <pandueman7@gmail.com>
2025-12-11 17:58:44 +0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,35 @@
-const mysql = require('mysql2')
-const dotenv = require('dotenv')
-dotenv.config()
+const db = require('../config/db')
+//const { addBook } = require('../controllers/userController')
 
-const pool = mysql.createPool({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
-})
+//async - wait
+const getAllBooks = async () =>{
+    const [rows] = await db.query("select * from buku")
+    return rows
+}
 
-const poolPromise = pool.promise()
+const getBookByCode = async(code)=>{
+    const [row] =
+    await db.query("select * from buku where kode_buku=?",[code])
+    return row[0]
+}
+const addBook = async(book) =>{
+    const {kode, judul, pengarang, penerbit} = book
+    const query = "insert into buku" + "(kode_buku,judul,pengarang,penerbit)" + "values (?,?,?,?)"
+    const affected = await db.query (query, [kode, judul, pengarang, penerbit])
+    return affected[0].affectedRows
+}
 
-module.exports = poolPromise
+const delBook = async(id)=>{
+    const aff = await db.query("Delete from buku where kode_buku=?", [id])
+    return aff[0].affectedRows
+}
+
+const updateBook = async (kode, book) => {
+    const { judul, pengarang, penerbit } = book
+    const query = "UPDATE buku " + "SET judul = ?, pengarang = ?, penerbit = ? " + "WHERE kode_buku = ?"
+    const affected = await db.query(query, [judul, pengarang, penerbit, kode])
+    return affected[0].affectedRows
+}
+
+
+module.exports = {getAllBooks,getBookByCode,addBook,delBook,updateBook}
